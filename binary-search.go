ce5c16e932f533befe0bc6357276243afdc2d76a
Zach Orr <zachorr139@gmail.com>
2013-09-12 10:03:05 -0400

    Added a linear search - ya know, for the kids

--- binary-search.go
+++ binary-search.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func sortedBinarySearch(sortedArray []int, toFind int) int {
+func binarySearch(arrayzor []int, toFind int) int {
 
   var low, high int
-  low = sortedArray[0]
-  high = sortedArray[len(sortedArray) - 1]
+  low = arrayzor[0]
+  high = arrayzor[len(arrayzor) - 1]
 
   if toFind > high || toFind < low {
     return -1
@@ -15,13 +15,13 @@ func sortedBinarySearch(sortedArray []int, toFind int) int {
   for low <= high {
     mid := low + (high - low)/2
     switch {
-      case toFind < sortedArray[mid]:
+      case toFind < arrayzor[mid]:
         high = mid - 1
       
-      case toFind > sortedArray[mid]:
+      case toFind > arrayzor[mid]:
         low = mid + 1
       
-      case toFind == sortedArray[mid]:
+      case toFind == arrayzor[mid]:
         return mid
     }
   }
@@ -30,25 +30,14 @@ func sortedBinarySearch(sortedArray []int, toFind int) int {
 
 func main() {
   
-  // Sorted binary search test
-  fmt.Println("Sorted binary search:")
-  sortedArray := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
-  index := sortedBinarySearch(sortedArray, 10)
+  // Binary search test
+  fmt.Println("Binary search:")
+  arrayzor := []int{0, 2, 4, 6, 8, 10, 12, 14, 16, 18}
+  index := binarySearch(arrayzor, 10)
   if index == -1 {
     fmt.Println("Number not found")
   } else {
     fmt.Println("Index: ", index)
-    fmt.Println("sortedArray[", index, "] = ", sortedArray[index])
-  }
-
-  // Unsorted binary search test
-  fmt.Println("Unsorted binary search:")
-  unsortedArray := []int{0, 4, 2, 8, 6, 12, 10, 16, 14, 18}
-  index = unsortedBinarySearch(unsortedArray, 10)
-  if index == -1 {
-    fmt.Println("Number not found")
-  } else {
-    fmt.Println("Index: ", index)
-    fmt.Println("unsortedArray[", index, "] = ", unsortedArray[index])
+    fmt.Println("arrayzor[", index, "] = ", arrayzor[index])
   }
 }
