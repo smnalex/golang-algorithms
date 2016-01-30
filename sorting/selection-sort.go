8b9aec6d22645743692cc70ca060fe443f1bac99
Alex Simion <smnalex@users.noreply.github.com>
2016-01-30 09:47:59 +0000

    Update selection-sort.go
    Fixed off-by-one error.

--- sorting/selection-sort.go
+++ sorting/selection-sort.go
@@ -7,9 +7,9 @@ import (
 
 func selectionSort(array []int) {
 	
-	for i := 0; i < len(array) - 1; i++ {
+	for i := 0; i < len(array); i++ {
 		min := i
-		for j := i + 1; j < len(array) - 1; j++ {
+		for j := i + 1; j < len(array); j++ {
 			if array[j] < array[min] {
 				min = j
 			}
